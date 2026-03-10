3bb33eaf19eab59ff9950e175008812d961a11d1
Billy Murphy <G00465240@atu.ie>
2026-03-10 19:44:30 +0000

    User login model made

--- src/main/java/ie/atu/oop/week3/gym_trainer_booking_manager/Controller/LogininController.java
+++
@@ -1,4 +0,0 @@
-package ie.atu.oop.week3.gym_trainer_booking_manager.Controller;
-
-public class LogininController {
-}
---
+++ src/main/java/ie/atu/oop/week3/gym_trainer_booking_manager/Service/UserLoginService.java
@@ -0,0 +1,7 @@
+package ie.atu.oop.week3.gym_trainer_booking_manager.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserLoginService {
+}
