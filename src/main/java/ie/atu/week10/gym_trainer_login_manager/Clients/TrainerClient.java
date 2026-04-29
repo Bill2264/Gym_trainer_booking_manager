3e3dd97f3bff291eca2fe665d9bd07fa1778b7ce
Billy Murphy <G00465240@atu.ie>
2026-04-29 16:37:41 +0100

    FeignClient for trainer and user setup

--- src/main/java/ie/atu/week10/gym_trainer_login_manager/Clients/TrainerClient.java
+++ src/main/java/ie/atu/week10/gym_trainer_login_manager/Clients/TrainerClient.java
@@ -1,4 +1,12 @@
 package ie.atu.week10.gym_trainer_login_manager.Clients;
 
-public class TrainerClient {
+import ie.atu.week10.gym_trainer_login_manager.Model.TrainerDetails;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name="Trainer-service", url="http://localhost:8080")
+public interface TrainerClient {
+    @GetMapping("/Trainer/{trainerName}")
+    TrainerDetails getTrainerDetails(@PathVariable String trainerName);
 }
