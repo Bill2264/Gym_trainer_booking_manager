2fafff9057220927a47232593a1165b1e7ada872
Billy Murphy <G00465240@atu.ie>
2026-04-29 23:09:42 +0100

    Login Controller done

---
+++ src/main/java/ie/atu/week10/gym_trainer_login_manager/Model/BookingHolder.java
@@ -0,0 +1,11 @@
+package ie.atu.week10.gym_trainer_login_manager.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookingHolder {
+    private Booking booking;
+    private UserDetails userDetails;
+}
